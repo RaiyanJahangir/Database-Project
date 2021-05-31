4472c1bdac2c95bd9bdeb4c83d02808eede0a483
mumu <afnanalauddin30@gmail.com>
2021-05-31 23:34:24 +0600

    organization signup process

--- Organizationsignup.php
+++ Organizationsignup.php
@@ -7,18 +7,18 @@
                     <div class="section-title">
                         <h3 style="text-align: center;">Organization <span>Sign Up</span></h3>
                     </div>
-                    <form name="contact_form" class="default-form contact-form" action="sendmail.php" method="post">
+                    <form name="contact_form" class="default-form contact-form" action="process-organization-sign-up.php" method="post">
                         <div class="row">
                             <div class="col-md-12">
                                 <p>Organization Name</p>
                                 <div class="form-group">
-                                    <input type="text" name="Name" placeholder="Organization Name" required>
+                                    <input type="text" name="organization-name" placeholder="Organization Name" required>
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <p>Organization Contact</p>
                                 <div class="form-group">
-                                    <input type="text" name="contact" placeholder="Organization Contact" required>
+                                    <input type="text" name="organization-contact" placeholder="Organization Contact" required>
                                 </div>
                             </div>
                             <div class="col-md-12">
@@ -26,44 +26,44 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" name="city" placeholder="city" required>
+                                    <input type="text" name="organization-city" placeholder="city" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" name="street" placeholder="Street" required>
+                                    <input type="text" name="organization-street" placeholder="street" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" name="postal" placeholder="postal" required>
+                                    <input type="text" name="organization-postal" placeholder="postal" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" name="branch" placeholder="Branch" required>
+                                    <input type="text" name="organization-branch" placeholder="branch" required>
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <p>Password</p>
-                                    <input type="confirmpass" id="myInput" required>
+                                    <input type="password" id="myInput" name="pass" required>
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <p>Confirm Password</p>
-                                    <input type="pass" id="myInput" required>
+                                    <input type="password" id="myInput" required>
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12 col-xs-12">
                                 <div class="form-group text-center">
-                                    <button type="submit" class="btn-style-one">Create Account</button>
+                                    <button type="submit" class="btn-style-one" name="save">Create Account</button>
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12 col-xs-12">
                                 <p>Already Registered?
-                                <a href="#" class="text-primary ml-2" style="color: red;">Login</a></p>
+                                <a href="login.php" class="text-primary ml-2" style="color: red;">Login</a></p>
                             </div>
                         </div>
                     </form>
---
+++ process-organization-sign-up.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'database.php';
+if(isset($_POST['save']))
+{	 
+    $nam=$_POST['organization-name'];
+    $email=$_POST['organization-contact'];
+    $address_street=$_POST['organization-street'];
+    $address_branch=$_POST['organization-branch'];
+    $address_postal=$_POST['organization-postal'];
+    $address_city=$_POST['organization-city'];
+    $pass=$_POST['pass'];
+	$query = oci_parse($conn, "INSERT INTO organization(org_contact,org_city,org_postal,org_branch,org_street,org_name,org_pass) values ('$email','$address_city','$address_postal','$address_branch','$address_street','$nam','$pass')");
+	$result = oci_execute($query);
+	if ($result) {
+				echo "Registered successfully !";
+				include 'login.php';
+				exit();
+	}
+	else{
+		echo "Error !";
+				exit();
+	}
+}
+?>
